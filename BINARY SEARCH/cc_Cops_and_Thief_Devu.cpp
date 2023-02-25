b6472d7062728052aee21175d92bd200e3087823
sukhmani1303 <sukhmani1303@gmail.com>
2023-02-26 03:26:13 +0530

    [code chef] Cops & Theif Devu

---
+++ BINARY SEARCH/cc_Cops_and_Thief_Devu.cpp
@@ -0,0 +1,122 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>  
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+   
+using namespace std;
+   
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+// ll MOD = 998244353;
+double eps = 1e-12;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#define ar array
+#define sza(x) ((int)x.size())
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+   
+  
+void solve(){
+
+    int m,x,y;
+    cin>>m>>x>>y;
+    int cops[m];
+
+    int max_house = x*y;
+
+    for(int i = 0; i<m; i++ ){
+        cin>>cops[i];
+    }
+
+    int houses[101] = {0};
+
+    for(int i = 0; i<m; i++){
+
+        int left_house = 1, right_house = 100; // occupied houses
+
+        // leftmost
+        if(cops[i] - max_house>0){
+            left_house = cops[i] - max_house;
+        }
+        if(cops[i] + max_house<100){
+            right_house = cops[i] + max_house;
+        }
+
+        // mark houses
+
+        for(int i = left_house; i<=right_house; i++){
+            houses[i] = 1;
+        }
+    }
+
+    int cnt = 0;
+
+    for( int i =1; i<=101; i++){
+        if (houses[i] ==1 ){
+            cnt++;
+        }
+    } 
+
+    cout<<100-cnt<<ln;   
+
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+        solve();
+    }
+    return 0;
+}
